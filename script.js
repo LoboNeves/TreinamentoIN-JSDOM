f7acfa3f7f03ab8ae372be74f5f3e49e760ccded
Daniel Neves <dbenditoneves@gmail.com>
2020-08-05 11:59:05 -0300

    Feito a tarefa principal, sem extras

---
+++ script.js
@@ -0,0 +1,12 @@
+const lista = document.getElementById("lista");
+
+function printnome() {
+    const nome = document.createElement('li');
+    nome.innerText = document.getElementById("Mytext").value;
+    lista.appendChild(nome);
+    document.getElementById("Mytext").value = ''
+}
+
+lista.addEventListener('click', function(e) {
+    this.removeChild(e.target);
+})
