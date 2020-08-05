1b0b26ae580fb42e6864f9d8395706b94ca3febe
Daniel Neves <dbenditoneves@gmail.com>
2020-08-05 12:12:44 -0300

    Adicionado extra de editar ao selecionar um nome

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const lista = document.getElementById("lista");
 function printnome() {
     const nome = document.createElement('li');
     nome.innerText = document.getElementById("Mytext").value;
+    nome.contentEditable = true;
     lista.appendChild(nome);
     document.getElementById("Mytext").value = ''
 }
